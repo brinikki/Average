fa7d4b36bdc48fb68952452a1dca737c436f2353
ieee754 <94340126+brinikki@users.noreply.github.com>
2022-09-23 14:26:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Type 5 numbers you want to see the average of")
+
+print("Enter Number 1")
+gn = input()
+print("Enter Number 2")
+gn2 = input()
+print("Enter Number 3")
+gn3 = input()
+print("Enter Number 4")
+gn4 = input()
+print("Enter Number 5")
+gn5 = input()
+
+add = int(gn) + int(gn2) + int(gn3) + int(gn4) +int(gn5) 
+
+mean = add/5
+
+print ("\n")
+print (mean)
